184432c14b896c8d31f6192302c29b8dffdb7cc5
kaylawatson172 <124411095+kaylawatson172@users.noreply.github.com>
2023-04-02 03:30:33 -0400

    Update Random_Password_Generator.py

--- Random_Password_Generator.py
+++ Random_Password_Generator.py
@@ -1,8 +1,8 @@
 # Creating a random password generator in Python
 
 # Step 1: Import necessary Python libraries
-import secrets
-import string
+import secrets # Used the secrets library rather than the random library to generate cryptographically secure passwords
+import string # Used the strings library to generate all character possibilities by type rather than individually typing them 
 
 # Step 2: Define all character possibilities (letters, digits, special characters)
 letters = string.ascii_letters # ascii_letters is the concatenation of all upper and lowercase letters
@@ -12,12 +12,12 @@ special_chars = string.punctuation # punctuation is a string of all special char
 # Step 3: Concatenate the 3 strings from step 2 and store them in a variable
 characters = letters + digits + special_chars
 
-# Step 4: Store the desired length of the password in a variable
+# Step 4: Store the desired length of the password in a variable (Secure passwords are long to decrease predictability 
 password_length = 12
 
 # Step 5: Generate a password string
 password = ''
-for i in range(password_length):
+for i in range(password_length): # Used a for loop to repeat sampling of random characters
     password += ''.join(secrets.choice(characters))
 
 # Step 6: Generate a password given constraints
